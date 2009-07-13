568a3429d004e85955d58b9f66d4f526c6a2cc1b
Gregory Todd Williams <greg@evilfunhouse.com>
2009-07-13 15:04:55 -0400

    - Updated examples/mpi.c to use new async IO code to distribute triples.

--- examples/mpi.c
+++ examples/mpi.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include "mpi.h"
 #include "hexastore.h"
 #include "mergejoin.h"
 #include "node.h"
 #include "bgp.h"
+#include "async_des.h"
 
 typedef struct {
-	int allocated;
-	int count;
-	hx_node_id* items;
-} triple_set_t;
-triple_set_t* new_triple_set ( int size );
-int free_triple_set ( triple_set_t* c, int free_contained_objects );
-void triple_set_push_item( triple_set_t* set, hx_node_id t );
-void triple_set_unshift_item( triple_set_t* set, hx_node_id t );
+	hx_hexastore* hx;
+	hx_storage_manager* st;
+	hx_index_iter* iter;
+	hx_nodemap* map;
+} send_args_t;
 
-int distribute_triples ( hx_hexastore* hx, hx_storage_manager* s );
-int receive_triples ( hx_hexastore* hx, hx_storage_manager* s );
+typedef struct {
+	hx_hexastore* hx;
+	hx_storage_manager* st;
+} recv_args_t;
+	
+int send_handler(async_mpi_session* ses, void* args);
+int recv_handler(async_mpi_session* ses, void* args);
 
 int main ( int argc, char** argv ) {
 	int mysize, myrank;
@@ -24,9 +26,11 @@ int main ( int argc, char** argv ) {
 	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	
-	hx_hexastore* hx;
-	hx_storage_manager* s	= hx_new_memory_storage_manager();
+	hx_storage_manager* st	= hx_new_memory_storage_manager();
+	hx_hexastore* hx		= hx_new_hexastore( st );
 	
+	hx_nodemap* map		= NULL;
+	hx_index_iter* iter	= NULL;
 	if (myrank == 0) {
 		const char* filename	= argv[1];
 		FILE* f	= fopen( filename, "r" );
@@ -34,15 +38,45 @@ int main ( int argc, char** argv ) {
 			perror( "Failed to open hexastore file for reading: " );
 			MPI_Abort(MPI_COMM_WORLD, 1);
 		}
-		hx					= hx_read( s, f, 0 );
+		hx_hexastore* source_data	= hx_read( st, f, 0 );
 		fprintf( stderr, "Finished loading hexastore...\n" );
-		
-		// send triples to the right nodes
-		distribute_triples( hx, s );
-	} else {
-		hx					= hx_new_hexastore( s );
-		receive_triples( hx, s );
+		map			= hx_get_nodemap( source_data );
+		hx_node* s	= hx_new_variable( hx );
+		hx_node* p	= hx_new_variable( hx );
+		hx_node* o	= hx_new_variable( hx );
+		iter		= hx_get_statements( source_data, st, s, p, o, HX_SUBJECT );
 	}
+	
+	send_args_t send_args;
+	send_args.hx		= hx;
+	send_args.st		= st;
+	send_args.iter		= iter;
+	send_args.map		= map;
+	
+	recv_args_t recv_args;
+	recv_args.hx	= hx;
+	recv_args.st	= st;
+	
+	int msg_size			= 3 * sizeof(hx_node_id);
+	int num_sends			= (myrank == 0) ? 4 : 0;
+	async_des_session* ses	= async_des_session_create(num_sends, &send_handler, &send_args, 20, &recv_handler, &recv_args, msg_size);
+	
+//	fprintf( stderr, "beginning async IO\n" );
+	while(async_des(ses) == ASYNC_PENDING) {
+		// continue until finished
+	}
+//	fprintf( stderr, "done with async IO\n" );
+	
+	if (myrank == 0) {
+		hx_free_index_iter(iter);
+	}
+	
+	int size	= (int) hx_triples_count( hx, st );
+	fprintf( stderr, "node %d has %d triples\n", myrank, size );
+	async_des_session_destroy(ses);
+	
+	
+/**	
 	hx_nodemap* map		= hx_get_nodemap( hx );
 	
 	hx_node* x			= hx_new_named_variable( hx, "x" );
@@ -113,145 +147,80 @@ int main ( int argc, char** argv ) {
 	hx_free_node( person );
 	hx_free_node( knows );
 	hx_free_node( name );
-	hx_free_hexastore( hx, s );
-	hx_free_storage_manager( s );
+	**/
 	
+	hx_free_hexastore( hx, st );
+	hx_free_storage_manager( st );
 	MPI_Finalize(); 
 	return 0;
 }
 
-int distribute_triples ( hx_hexastore* hx, hx_storage_manager* st ) {
+
+// int distribute_triples ( hx_hexastore* hx, hx_storage_manager* st, async_des_session* ses ) {
+int send_handler(async_mpi_session* ses, void* args) {
+	send_args_t* send_args	= (send_args_t*) args;
+	hx_hexastore* hx		= send_args->hx;
+	hx_storage_manager* st	= send_args->st;
+	hx_index_iter* iter		= send_args->iter;
+	
 	int mysize, myrank;
 	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	
-	hx_node* s	= hx_new_variable( hx );
-	hx_node* p	= hx_new_variable( hx );
-	hx_node* o	= hx_new_variable( hx );
-	hx_index_iter* iter	= hx_get_statements( hx, st, s, p, o, HX_SUBJECT );
+	if (myrank != 0) {
+		// only the head node can send triples to other nodes
+		return 0;
+	}
 	
-	int counter	= 0;
-	int *triple_counts	= calloc( mysize, sizeof( int ) );
 	hx_node_id nodes[3];
 	while (!hx_index_iter_finished(iter)) {
-		hx_index_iter_current ( iter, &(nodes[0]), &(nodes[1]), &(nodes[2]) );
+		hx_index_iter_current( iter, &(nodes[0]), &(nodes[1]), &(nodes[2]) );
 		int hash	= (nodes[0] ^ nodes[1] ^ nodes[2]) % mysize;
 		
+		char *string;
+		hx_triple_id t;
+		t.subject	= nodes[0];
+		t.predicate	= nodes[1];
+		t.object	= nodes[2];
+		hx_triple_id_string( &t, send_args->map, &string );
+//		fprintf( stderr, "%s hashed to node %d\n", string, hash );
+		free(string);
+		
 		if (hash > 0) {
-			// if the triple hashes to a node other than this (master) node, send it out
-//			fprintf( stderr, "sending triple { %d, %d, %d } to node %d\n", (int) nodes[0], (int) nodes[1], (int) nodes[2], hash );
-			int rc	= MPI_Send( nodes, 3 * sizeof(hx_node_id), MPI_BYTE, hash, 1, MPI_COMM_WORLD );
-			triple_counts[hash]++;
+//			fprintf( stderr, "- sending triple to node %d\n", hash );
+			int size			= 3 * sizeof( hx_node_id );
+			hx_node_id* buffer	= malloc( size );
+			if (buffer == NULL) {
+				fprintf( stderr, "*** malloc failed in mpi.c:send_handler\n" );
+			}
+			memcpy( buffer, nodes, size );
+			async_mpi_session_reset3(ses, buffer, size, hash, ses->flags | ASYNC_MPI_FLAG_FREE_BUF);
+			hx_index_iter_next(iter);
+			return 1;
 		} else {
+//			fprintf( stderr, "- adding triple to local triplestore\n" );
 			// do something with the triple to keep it on this node
-			counter++;
-		}
-		hx_index_iter_next(iter);
-	}
-	
-// 	fprintf( stderr, "master handled %d triples\n", counter );
-// 	fprintf( stderr, "broadcasting end-of-stream triple\n" );
-	fprintf( stdout, "node %d recieved %d triples\n", myrank, counter );
-	for (int i = 1; i < mysize; i++) {
-		nodes[0]	= (hx_node_id) 0;
-		nodes[1]	= (hx_node_id) triple_counts[i];
-		int rc	= MPI_Send( nodes, 3 * sizeof(hx_node_id), MPI_BYTE, i, 1, MPI_COMM_WORLD );
-	}
-	hx_free_index_iter(iter);
-	return counter;
-}
-
-int receive_triples ( hx_hexastore* hx, hx_storage_manager* st ) {
-	int mysize, myrank;
-	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	
-	int total	= -1;
-	int counter	= 0;
-	while (counter != total) {
-		MPI_Status status;
-		hx_node_id nodes[3];
-		int rc	= MPI_Recv(nodes, 3 * sizeof(hx_node_id), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-//		fprintf( stderr, "node %d got triple: { %d, %d, %d }\n", myrank, (int) nodes[0], (int) nodes[1], (int) nodes[2] );
-		if (nodes[0] == (hx_node_id) 0) {
-			// this is the end-of-stream marker.
-			// nodes[1] contains the number of triples we've been sent.
-			fprintf( stdout, "node %d recieved end-of-stream marker with %d expected triples (currently got %d)\n", myrank, (int) nodes[1], counter );
-			total	= nodes[1];
-		} else {
-			counter++;
+			hx_add_triple_id( hx, st, nodes[0], nodes[1], nodes[2] );
+			hx_index_iter_next(iter);
 		}
 	}
-	return counter;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-triple_set_t* new_triple_set ( int size ) {
-	triple_set_t* container	= (triple_set_t*) calloc( 1, sizeof( triple_set_t ) );
-	container->allocated	= size;
-	container->count		= 0;
-	container->items		= (hx_node_id*) calloc( container->allocated, sizeof( void* ) );
-	return container;
-}
-
-int free_triple_set ( triple_set_t* c, int free_contained_objects ) {
-	free(c->items);
-	free(c);
+	
 	return 0;
 }
 
-void triple_set_push_item( triple_set_t* set, hx_node_id t ) {
-	if (set->allocated <= (set->count + 1)) {
-		int i;
-		hx_node_id* old;
-		hx_node_id* newlist;
-		set->allocated	*= 2;
-		newlist	= (hx_node_id*) calloc( set->allocated, sizeof( hx_node_id ) );
-		for (i = 0; i < set->count; i++) {
-			newlist[i]	= set->items[i];
-		}
-		old	= set->items;
-		set->items	= newlist;
-		free( old );
-	}
+// int receive_triples ( hx_hexastore* hx, hx_storage_manager* st, async_des_session* ses ) {
+int recv_handler(async_mpi_session* ses, void* args) {
+	recv_args_t* recv_args	= (recv_args_t*) args;
+	hx_hexastore* hx		= recv_args->hx;
+	hx_storage_manager* st	= recv_args->st;
 	
-	set->items[ set->count++ ]	= t;
+	int mysize, myrank;
+	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	
+	hx_node_id* nodes	= (hx_node_id*) ses->buf;
+	hx_add_triple_id( hx, st, nodes[0], nodes[1], nodes[2] );
+//	fprintf( stderr, "node %d got triple: { %d, %d, %d }\n", myrank, (int) nodes[0], (int) nodes[1], (int) nodes[2] );
+	return 1;
 }
 
-void triple_set_unshift_item( triple_set_t* set, hx_node_id t ) {
-	if (set->allocated <= (set->count + 1)) {
-		int i;
-		hx_node_id* old;
-		hx_node_id* newlist;
-		set->allocated	*= 2;
-		newlist	= (hx_node_id*) calloc( set->allocated, sizeof( hx_node_id ) );
-		for (i = 0; i < set->count; i++) {
-			newlist[i+1]	= set->items[i];
-		}
-		old	= set->items;
-		set->items	= newlist;
-		free( old );
-	} else {
-		int i;
-		for (i = set->count; i > 0; i--) {
-			set->items[i]	= set->items[i-1];
-		}
-	}
-	
-	set->count++;
-	set->items[ 0 ]	= t;
-}
