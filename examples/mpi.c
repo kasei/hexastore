0b1113290945eb3dd152c359eb02d83d58a7bfe8
Gregory Todd Williams <greg@evilfunhouse.com>
2009-07-15 19:34:36 -0400

    - Updated examples/mpi.c to use hx_parallel_distribute_triples_from_hexastore().

--- examples/mpi.c
+++ examples/mpi.c
@@ -3,22 +3,8 @@
 #include "mergejoin.h"
 #include "node.h"
 #include "bgp.h"
-#include "async_des.h"
+#include "parallel.h"
 
-typedef struct {
-	hx_hexastore* hx;
-	hx_storage_manager* st;
-	hx_index_iter* iter;
-	hx_nodemap* map;
-} send_args_t;
-
-typedef struct {
-	hx_hexastore* hx;
-	hx_storage_manager* st;
-} recv_args_t;
-	
-int send_handler(async_mpi_session* ses, void* args);
-int recv_handler(async_mpi_session* ses, void* args);
 int distribute_triples_from_file ( hx_hexastore* hx, hx_storage_manager* st, const char* filename );
 
 int main ( int argc, char** argv ) {
@@ -112,117 +98,20 @@ int distribute_triples_from_file ( hx_hexastore* hx, hx_storage_manager* st, con
 	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	
-	hx_nodemap* map		= NULL;
-	hx_index_iter* iter	= NULL;
+	hx_nodemap* map			= NULL;
+	hx_index_iter* iter		= NULL;
+	hx_hexastore* source	= NULL;
 	if (myrank == 0) {
 		FILE* f	= fopen( filename, "r" );
 		if (f == NULL) {
 			perror( "Failed to open hexastore file for reading: " );
 			MPI_Abort(MPI_COMM_WORLD, 1);
 		}
-		hx_hexastore* source_data	= hx_read( st, f, 0 );
+		source	= hx_read( st, f, 0 );
 		fprintf( stderr, "Finished loading hexastore...\n" );
-		map			= hx_get_nodemap( source_data );
-		hx_node* s	= hx_new_variable( hx );
-		hx_node* p	= hx_new_variable( hx );
-		hx_node* o	= hx_new_variable( hx );
-		iter		= hx_get_statements( source_data, st, s, p, o, HX_SUBJECT );
 	}
 	
-	send_args_t send_args;
-	send_args.hx		= hx;
-	send_args.st		= st;
-	send_args.iter		= iter;
-	send_args.map		= map;
-	
-	recv_args_t recv_args;
-	recv_args.hx	= hx;
-	recv_args.st	= st;
-	
-	int msg_size			= 3 * sizeof(hx_node_id);
-	int num_sends			= (myrank == 0) ? 4 : 0;
-	async_des_session* ses	= async_des_session_create(num_sends, &send_handler, &send_args, 20, &recv_handler, &recv_args, msg_size);
-	
-//	fprintf( stderr, "beginning async IO\n" );
-	while (async_des(ses) == ASYNC_PENDING) {
-		// continue until finished
-	}
-//	fprintf( stderr, "done with async IO\n" );
-	
-	if (myrank == 0) {
-		hx_free_index_iter(iter);
-	}
-	
-	int size	= (int) hx_triples_count( hx, st );
-	fprintf( stderr, "node %d has %d triples\n", myrank, size );
-	async_des_session_destroy(ses);
-	
-	return 1;
-}
-
-int send_handler(async_mpi_session* ses, void* args) {
-	send_args_t* send_args	= (send_args_t*) args;
-	hx_hexastore* hx		= send_args->hx;
-	hx_storage_manager* st	= send_args->st;
-	hx_index_iter* iter		= send_args->iter;
-	
-	int mysize, myrank;
-	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	
-	if (myrank != 0) {
-		// only the head node can send triples to other nodes
-		return 0;
-	}
-	
-	hx_node_id nodes[3];
-	while (!hx_index_iter_finished(iter)) {
-		hx_index_iter_current( iter, &(nodes[0]), &(nodes[1]), &(nodes[2]) );
-		int hash	= (nodes[0] ^ nodes[1] ^ nodes[2]) % mysize;
-		
-		char *string;
-		hx_triple_id t;
-		t.subject	= nodes[0];
-		t.predicate	= nodes[1];
-		t.object	= nodes[2];
-		hx_triple_id_string( &t, send_args->map, &string );
-//		fprintf( stderr, "%s hashed to node %d\n", string, hash );
-		free(string);
-		
-		if (hash > 0) {
-//			fprintf( stderr, "- sending triple to node %d\n", hash );
-			int size			= 3 * sizeof( hx_node_id );
-			hx_node_id* buffer	= malloc( size );
-			if (buffer == NULL) {
-				fprintf( stderr, "*** malloc failed in mpi.c:send_handler\n" );
-			}
-			memcpy( buffer, nodes, size );
-			async_mpi_session_reset3(ses, buffer, size, hash, ses->flags | ASYNC_MPI_FLAG_FREE_BUF);
-			hx_index_iter_next(iter);
-			return 1;
-		} else {
-//			fprintf( stderr, "- adding triple to local triplestore\n" );
-			// do something with the triple to keep it on this node
-			hx_add_triple_id( hx, st, nodes[0], nodes[1], nodes[2] );
-			hx_index_iter_next(iter);
-		}
-	}
-	
-	return 0;
-}
-
-int recv_handler(async_mpi_session* ses, void* args) {
-	recv_args_t* recv_args	= (recv_args_t*) args;
-	hx_hexastore* hx		= recv_args->hx;
-	hx_storage_manager* st	= recv_args->st;
-	
-	int mysize, myrank;
-	MPI_Comm_size(MPI_COMM_WORLD, &mysize);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	
-	hx_node_id* nodes	= (hx_node_id*) ses->buf;
-	hx_add_triple_id( hx, st, nodes[0], nodes[1], nodes[2] );
-//	fprintf( stderr, "node %d got triple: { %d, %d, %d }\n", myrank, (int) nodes[0], (int) nodes[1], (int) nodes[2] );
+	hx_parallel_distribute_triples_from_hexastore( 0, source, st, hx );
 	return 1;
 }
 
