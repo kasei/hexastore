d31e80268eccc27a26d847d1b9d8d79c03849a94
Gregory Todd Williams <greg@evilfunhouse.com>
2009-08-03 22:18:54 -0400

    - Added printf formatting defines in hexastore_types.h.

--- hexastore_types.h
+++ hexastore_types.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -14,6 +15,9 @@ extern "C" {
 #define HEAD_TREE_BRANCHING_SIZE				252
 #define VECTOR_TREE_BRANCHING_SIZE				28
 #define TERMINAL_TREE_BRANCHING_SIZE			4
+
+#define PRIuHXID	PRIu64
+#define PRIuHXID	PRIx64
 
 typedef int64_t hx_node_id;
 typedef hx_node_id list_size_t;
