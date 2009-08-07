fee4fd78004e19c7ac9e159fe5839f45fe289784
Gregory Todd Williams <greg@evilfunhouse.com>
2009-08-07 15:46:18 -0400

    - Added mpi_rdfio_readids to load pre-processed RDF triple data with integer node IDs.

--- mpi_rdfio.h
+++ mpi_rdfio.h
@@ -5,5 +5,6 @@
 #include "hexastore.h"
 
 int mpi_rdfio_readnt(char *filename, char *mapfilename, size_t bufsize, hx_hexastore **store, hx_storage_manager **manager, MPI_Comm comm);
+int mpi_rdfio_readids(char *filename, size_t bufsize, hx_hexastore **store, hx_storage_manager **manager, MPI_Comm comm);
 
 #endif /* _MPI_RDFIO_H */
