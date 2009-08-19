e01f15e9c5d18c01ffd37deba530b7ab6fbfc7b0
Gregory Todd Williams <greg@evilfunhouse.com>
2009-08-18 21:23:33 -0400

    - Added hx_container_t struct and functions in util.c.

--- util.h
+++ util.h
@@ -16,11 +16,24 @@ extern "C" {
 #include <string.h>
 #include <unistd.h>
 
+typedef struct {
+	char type;
+	int allocated;
+	int count;
+	void** items;
+} hx_container_t;
+
 typedef uint64_t hx_hash_function ( const char* s );
 
 #include "hexastore_types.h"
 
 uint64_t hx_util_hash_string ( const char* s );
+hx_container_t* hx_new_container ( char type, int size );
+int hx_free_container ( hx_container_t* c );
+void hx_container_push_item( hx_container_t* set, void* t );
+void hx_container_unshift_item( hx_container_t* set, void* t );
+int hx_container_size( hx_container_t* c );
+void* hx_container_item ( hx_container_t* c, int i );
 
 #ifdef __cplusplus
 }
