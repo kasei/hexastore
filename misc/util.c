67fd4c58272bae11a09d987769a960781c2b8dbf
Gregory Todd Williams <greg@evilfunhouse.com>
2009-08-29 16:24:28 -0400

    - Added LL modfier on long long integer constant in hx_util_hash_buffer.

--- misc/util.c
+++ misc/util.c
@@ -10,7 +10,7 @@ uint64_t hx_util_hash_buffer ( const char* s, size_t len ) {
 	int i;
 	for (i = 0; i < len; i++) {
 		unsigned char ki	= (unsigned char) s[i];
-		uint64_t highorder	= h & 0xfffffffff8000000;	// extract high-order 37 bits from h
+		uint64_t highorder	= h & 0xfffffffff8000000LL;	// extract high-order 37 bits from h
 		h	= h << 37;									// shift h left by 37 bits
 		h	= h ^ (highorder >> 37);					// move the highorder 37 bits to the low-order end and XOR into h
 		h = h ^ ki;										// XOR h and ki
